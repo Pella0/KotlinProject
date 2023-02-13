02462284e733d64d6d3727a758831a6095142adc
Pella0 <67748582+Pella0@users.noreply.github.com>
2023-02-13 15:51:06 +0100

    create first activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ADD BUTTON
+        val button = findViewById<Button>(R.id.start_new_activity_btn)
+        button.setOnClickListener{
+            val intent = Intent(this, NewActivity::class.java)
+            startActivity(intent)
+        }
+
+        //LOG
+        Log.v("MainActivity", "Verbose message")
+        Log.d("MainActivity", "Debug Message")
+        Log.i(TAG, "Info message")
+        Log.w(TAG, "Warning message")
+        Log.e(TAG, "Error message")
     }
 }
